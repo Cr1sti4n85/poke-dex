1f0eb75290d6dcc15aca3d188dcb6bc0c5eeaeb3
Cristian Perez <cperezlecaros@gmail.com>
2024-11-26 16:01:33 -0700

    Build SideNav and add index.css

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,5 +1,22 @@
+import { first151Pokemon, getFullPokedexNumber } from "../utils";
+
 function SideNav() {
-  return <nav></nav>;
+  return (
+    <nav>
+      <div className="header">
+        <h1 className="text-gradient">Pokédex</h1>
+      </div>
+      <input type="text" />
+      {first151Pokemon.map((pokemon, pokeIndex) => {
+        return (
+          <button className={`nav-card`} key={pokeIndex}>
+            <p>{getFullPokedexNumber(pokeIndex)}</p>
+            <p>{pokemon}</p>
+          </button>
+        );
+      })}
+    </nav>
+  );
 }
 
 export default SideNav;
