530b96e4d960888d11b1359937aa414a791b6c61
Cristian Perez <cperezlecaros@gmail.com>
2024-12-03 17:02:50 -0700

    Implement filtering pokemon by name or index

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,16 +1,47 @@
+import { useState } from "react";
 import { first151Pokemon, getFullPokedexNumber } from "../utils";
 
-function SideNav() {
+function SideNav(props) {
+  const { selectedPokemon, setSelectedPokemon } = props;
+
+  const [searchValue, setSearchValue] = useState("");
+
+  const filteredPokemon = first151Pokemon.filter((el, elIndex) => {
+    if (getFullPokedexNumber(elIndex).includes(searchValue)) {
+      return true;
+    }
+
+    if (el.toLowerCase().includes(searchValue.toLowerCase())) return true;
+
+    return false;
+  });
   return (
     <nav>
       <div className="header">
         <h1 className="text-gradient">Pokédex</h1>
       </div>
-      <input type="text" />
-      {first151Pokemon.map((pokemon, pokeIndex) => {
+      <input
+        type="text"
+        value={searchValue}
+        onChange={(e) => {
+          setSearchValue(e.target.value);
+        }}
+      />
+      {filteredPokemon.map((pokemon, pokeIndex) => {
+        const truePokedexNumber = first151Pokemon.indexOf(pokemon);
+
         return (
-          <button className={`nav-card`} key={pokeIndex}>
-            <p>{getFullPokedexNumber(pokeIndex)}</p>
+          <button
+            onClick={() => {
+              setSelectedPokemon(truePokedexNumber);
+            }}
+            className={
+              `nav-card` +
+              (pokeIndex === selectedPokemon ? " nav-card-selected" : " ")
+            }
+            key={pokeIndex}
+          >
+            <p>{getFullPokedexNumber(truePokedexNumber)}</p>
             <p>{pokemon}</p>
           </button>
         );
